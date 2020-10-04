e837a13b568f22da6c03b803ab8096f8b3c8095f
DESMOND NRUEM ISAAC <isaac1436@student.babcock.edu.ng>
2020-10-04 17:07:45 +0000

    ISAAC DESMOND NRUEM 19/1436

--- Main.java
+++ Main.java
@@ -1,19 +1,72 @@
+import java.util.Arrays;
+
 class Main {
   public static void main(String[] args) {
-   // kindly use appropriate data types for your declaration
-   //declare an array variable and assign the days of the month debtor defaulted,
-   
-   // declare variables of amount the debtor is to pay per day, and amount the debtor paid and assign values to them
-   
-   // declare all the variables needed for your calculations
 
-   // calculate and print total amount the debtor is to pay using for each loop
+  // kindly use appropriate data types for your declaration
+  //declare an array variable and assign the days of the month debtor defaulted,
+  
+  byte[] daysDefaulted = {1,5,9,12,13,30}; 
 
-   // calculate the days the debtor paid for and print the days in which was paid for starting from the left to the right of the array. Also check if there was a day that the debtor did not pay the complete amount for that day, if yes, print the day and the amount left to pay for that day. 
+
+  // declare variables of amount the debtor is to pay per day, and amount the debtor paid and assign values to them
+
+  int amtPerDay=50000;
+
+  float amtPaid=1200000;
+  
+  // declare all the variables needed for your calculations
+
+  byte noOfDaysPaid;
+  
+  float amtLeft;
+
+  float amount;
+
+  byte val=1;
+  
+  byte val2=1;
+  
+  byte [] april = new byte[30];
+  
+  for (byte index = 1; index < 30 ; index++){
+    april[index] = val;
+    val++;
+  }    
+  
+  // calculate and print total amount the debtor is to pay using for each loop
+  
+  byte [] daysToPay = new byte[1];
+  
+  for (float amt : daysToPay){
+    amt=amtPerDay * val;
+    System.out.println("the total amount the debtor is to pay is $"+ amt);
+    
+  }
+  
+  // calculate the days the debtor paid for and print the days in which was paid for starting from the left to the right of the array. Also check if there was a day that the debtor did not pay the complete amount for that day, if yes, print the day and the amount left to pay for that day. 
+  
+  noOfDaysPaid=(byte)(amtPaid/amtPerDay);
+
+  System.out.println("the number of days paid for is "+noOfDaysPaid);
+
+  byte [] daysPaid = {2,3,4,6,7,8,10,11,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
+
+  System.out.println("the days paid for are the "+Arrays.toString(daysPaid));
+  
+  System.out.println("there were no days where the debtor did not pay the complete amount for that day");
 
    // calculate amount the debtor is left to pay if he did not pay complete fees and the days not paid for or  check if the debtor paid extra and the extra amount he paid for, or if he paid the exact amount needed to be paid and print likewise
+   
+   amount=amtPerDay * val;
 
-   //kindly remove the statement below when you are done with the assignment
-    System.out.println("Hello world!");
+   amtLeft=amount-amtPaid;
+   
+   System.out.println("the amount left for the debtor to pay is $"+amtLeft);
+   
+   System.out.println("the days not paid for are the "+Arrays.toString(daysDefaulted));
+
+  System.out.println("the debtor did not pay extra and he also did not pay the exact amount needed to be paid");
+  
   }
 }
