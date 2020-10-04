f29db255aee58dc7d19c96ee26fc4279e8c808c2
DESMOND NRUEM ISAAC <isaac1436@student.babcock.edu.ng>
2020-10-04 18:20:54 +0000

    ISAAC DESMOND NRUEM 19/1436
    I used an import package to print out the elements of my arrays on one line

--- Main.java
+++ Main.java
@@ -60,8 +60,6 @@ class Main {
 
   // calculate amount the debtor is left to pay if he did not pay complete fees and the days not paid for or  check if the debtor paid extra and the extra amount he paid for, or if he paid the exact amount needed to be paid and print likewise
    
-  amount=amtPerDay * val1;
-
   amtLeft=amount-amtPaid;
    
   System.out.println("the amount left for the debtor to pay is $"+amtLeft);
